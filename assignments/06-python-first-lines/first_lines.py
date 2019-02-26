b045adab1f28ab4558a9cd666a60c369fb811f11
Patarajarin Akarapipad <patarajarina@login3.cm.cluster>
2019-02-26 09:27:12 -0700

    homework6

---
+++ assignments/06-python-first-lines/first_lines.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""
+Author : patarajarina
+Date   : 2019-02-25
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import sys
+import os
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Argparse Python script',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        
+           
+    parser.add_argument(
+        'positional', metavar='DIR', help='A positional argument', nargs='+')
+
+#    parser.add_argument(
+#        'DIR',
+#        '--',
+#        help='A named string argument',
+#        metavar='DIR',
+#        type=dir,
+#        default=None,
+#        nargs='+',
+#        default='')
+
+    parser.add_argument(
+        '-w',
+        '--width',
+        help='A named integer argument',
+        metavar='int',
+        type=int,
+        
+        default=50)
+
+#    parser.add_argument(
+#        '-f', '--flag', help='A boolean flag', action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def warn(msg):
+    """Print a message to STDERR"""
+    print(msg, file=sys.stderr)
+
+
+# --------------------------------------------------
+def die(msg='Something bad happened'):
+    """warn() and exit with error"""
+    warn(msg)
+    sys.exit(1)
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+#    args = sys.argv[1:]
+ #   str_arg = args.arg
+    DIRS = args.positional
+#    flag_arg = args.flag
+    width = args.width
+    
+#    print(DIRS)
+#    dirname = args[0] #check
+    for dirname in DIRS:
+        if not os.path.isdir(dirname):
+            print('"{}" is not a directory'.format(dirname), file=sys.stderr)
+        else:
+    
+            for eachfile in os.listdir(dirname):
+                out={}
+                with open('{}/{}'.format(dirname,eachfile)) as temp:
+                    out[temp.readline().strip()]=temp
+                
+            for thelines in sorted(out.keys()):
+                
+                print('{} {} {}'.format(thelines, ['.' for j in width-len(thelines)+1],out.items()))  
+                                
+
+#    print('str_arg = "{}"'.format(str_arg))
+#    print('int_arg = "{}"'.format(int_arg))
+#    print('flag_arg = "{}"'.format(flag_arg))
+#    print('positional = "{}"'.format(pos_arg))
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
